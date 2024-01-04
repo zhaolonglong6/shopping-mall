945b83f304e367f936739cba72b63a22323ebf8d
zll <2551967774@qq.com>
2024-01-04 18:57:35 +0800

    首页

---
+++ src/stores/navigationStore.js
@@ -0,0 +1,13 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import { getCategoryAPI } from '../api/layout'
+
+export const useNavigationStore = defineStore('navigation', () => {
+  const navigationList = ref([])
+  const getCategory = async () => {
+    let res = await getCategoryAPI()
+    navigationList.value = res.data.result
+    console.log(navigationList.value, res);
+  }
+  return { navigationList, getCategory }
+})
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
+
+import { useNavigationStore } from '@/stores/navigationStore'
+import { onMounted } from 'vue'
+
+const navigationStore = useNavigationStore()
+onMounted(() => {
+  navigationStore.getCategory()
+})
+</script>
 
 <template>
-  <div>1</div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
+  <LayoutFixed />
 </template>
-
-<style scoped lang="less"></style>
